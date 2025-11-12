d726aae5b358d4db859583e1370a06befa09bbc7
Julien CANTAU <jcantau@europa-group.com>
2025-11-12 18:40:11 +0100

    Frontend : spinner

---
+++ frontend/web/src/app/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './loading-spinner.component.html',
+  styleUrls: ['./loading-spinner.component.scss'],
+})
+export class LoadingSpinnerComponent {
+  @Input() label = 'Chargement...';
+}
